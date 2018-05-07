48501e4e68b13e0e0b9ab8140bbd05705f9a28c2
Marketa Slukova <Em@n144-p78.eduroam.kth.se>
2018-05-07 14:32:23 +0200

    first draft, gets stuck on converter.parse line

---
+++ read-data.py
@@ -0,0 +1,34 @@
+import music21
+from music21 import *
+import glob
+
+def get_notes():
+    """ Get all the notes and chords from the midi files in the ./midi_songs directory """
+    notes = []
+
+    for file in glob.glob('data/*/*.mid'):
+        midi = converter.parse(file)
+
+        print("Parsing %s" % file)
+
+        notes_to_parse = None
+
+        try: # file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            notes_to_parse = s2.parts[0].recurse()
+        except: # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
+
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notes.append('.'.join(str(n) for n in element.normalOrder))
+
+    #with open('data/notes', 'wb') as filepath:
+        #pickle.dump(notes, filepath)
+
+    return notes
+
+x = get_notes()
+print(x)
